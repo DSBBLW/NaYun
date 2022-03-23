02a777a0dbe077753ab60475ae88e821289d3322
DSBBLW <3371959877@qq.com>
2022-03-23 14:09:45 +0800

    'maina'

---
+++ src/main/java/com/nayun/SpringBootApplication1.java
@@ -0,0 +1,12 @@
+package com.nayun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootApplication1 extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication1.class);
+    }
+}
